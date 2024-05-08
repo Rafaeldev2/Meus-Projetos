1f449bcffec0e190859038d811fcb1ef1edbb15c
Rafaeldev2 <116116255+Rafaeldev2@users.noreply.github.com>
2024-05-08 17:39:09 -0300

    Add files via upload

---
+++ HTML CLACULADORA/calculadora.js
@@ -0,0 +1,8 @@
+let num1 = document.getElementById("num1")
+let num2 = document.getElementById("num2")
+let resultado
+
+function calculadora(){
+    resultado = (Number(num1).valueOf) + Number((num2).valueOf)
+}
+console.log(resultado)
